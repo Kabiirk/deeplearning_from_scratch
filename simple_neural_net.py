eb9314e63dcd04c6770d6006829689a765ca897d
Kabiir <kabiirk@gmail.com>
2021-02-17 01:33:50 +0530

    Made a Simple 1 neuron network

--- simple_neural_net.py
+++ simple_neural_net.py
@@ -1,1 +1,12 @@
-print("Simple Neural Net !")
+weight = 0.1
+
+def neural_network(inp, weight):
+    prediction = inp*weight
+    return prediction
+
+number_of_entries = [8.5, 9.5, 10, 9]
+
+inp = number_of_entries[0]
+
+pred = neural_network(inp, weight)
+print(pred) # = 0.85
