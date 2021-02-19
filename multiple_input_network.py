77f727f8d3e5bf94b616b2fac08df82718c7bdd4
Kabiir <kabiirk@gmail.com>
2021-02-19 23:42:46 +0530

    added a numpy implementation as well

--- multiple_input_network.py
+++ multiple_input_network.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Predictions with multiple inputs
 weights = [0.1, 0.2, 0]
 
@@ -29,7 +31,7 @@ nfans = [1.2, 1.3, 0.5, 1.0]
 # for the first game of the season
 inp = [toes[0],wlrec[0],nfans[0]]
 pred = neural_network(inp,weights)
-print(pred)
+print(pred) # = 0.98
 
 
 # input   weight   local prediction
@@ -38,3 +40,23 @@ print(pred)
 # ( 1.20 * 0.0 ) = 0.00 = fans prediction
 # toes prediction + wlrec prediction + fans prediction = final prediction
 #       0.85      +     0.13         +     0.00        =     0.98
+
+
+# Now the same code but in Numpy
+
+weights = np.array([0.1, 0.2, 0])
+
+def neural_net(np_inp, np_weight):
+    pred = np_inp.dot(np_weight)
+    
+    return pred
+
+toes = np.array([8.5, 9.5, 9.9, 9.0])
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
+# input corresponds to every entry
+# for the first game of the season
+
+input = np.array([toes[0],wlrec[0],nfans[0]])
+pred = neural_network(input,weights)
+print(pred) # = 0.98
