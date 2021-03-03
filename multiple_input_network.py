cb5ce059822aa180e4834ca60b701ea2309cca01
Kabiir <kabiirk@gmail.com>
2021-03-03 22:40:18 +0530

    Added NN with multiple outputs

--- multiple_input_network.py
+++ multiple_input_network.py
@@ -60,3 +60,32 @@ nfans = np.array([1.2, 1.3, 0.5, 1.0])
 input = np.array([toes[0],wlrec[0],nfans[0]])
 pred = neural_network(input,weights)
 print(pred) # = 0.98
+
+# Prediction with multiple inputs
+'''
+ instead of predicting just
+whether the team won or lost,
+now we're also predicting whether
+they are happy/sad AND the percentage
+of the team that is hurt. We are
+making this prediction using only
+the current win/loss record 
+'''
+weights = [0.3, 0.2, 0.9]
+def ele_mul(number,vector):
+    output = [0,0,0]
+    assert(len(output) == len(vector))
+    for i in range(len(vector)):
+        output[i] = number * vector[i]
+    
+    return output
+
+def neural_network(input, weights):
+    pred = ele_mul(input,weights)
+    return pred
+
+wlrec = [0.65, 0.8, 0.8, 0.9]
+input = wlrec[0]
+pred = neural_network(input,weights)
+
+print(pred) # [0.195, 0.13, 0.5850000000000001]
