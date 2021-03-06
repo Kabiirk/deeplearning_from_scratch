8c1a15f5be407012808038c1c0803ddbf98362e4
Kabiir <kabiirk@gmail.com>
2021-03-06 22:56:10 +0530

    Corrcted vect_mat_mul & added dot methd for ease

--- predicition_multiple_output.py
+++ predicition_multiple_output.py
@@ -2,14 +2,20 @@ weights = [ [0.1, 0.1, -0.3],  #hurt?
             [0.1, 0.2, 0.0],   #win?
             [0.0, 1.3, 0.1] ]  #sad?  
             #toes %win #fans
+def dot(K, L):
+    if len(K) != len(L):
+        return 0
+    
+    return sum(i[0] * i[1] for i in zip(K, L))
 
 def vect_mat_mul(vect,matrix):
-    # assert(len(a) == len(b))
-    output = 0
-    for i in range(a):
-        output += (a[i] * b[i])
+    assert(len(vect) == len(matrix))
+    output_list = []
+
+    for i in range(len(matrix)):
+        output_list.append(dot(vect, matrix[i]))
     
-    return output
+    return output_list
 
 def neural_network(input_vect, weights):
     pred = vect_mat_mul(input_vect,weights)
@@ -36,7 +42,8 @@ pred = neural_network(inp,weights)
 
 print(pred)
 
+# We need dot method to multiple our 1D vector to each 1D list in out matrix
      #toes          %win          #fans
-# (8.5 * 0.1) + (0.65 * 0.1) + (1.2 * -0.3) = 0.555 = hurt prediction
-# (8.5 * 0.1) + (0.65 * 0.2) + (1.2 * 0.0) = 0.98 = win prediction
-# (8.5 * 0.0) + (0.65 * 1.3) + (1.2 * 0.1) = 0.965 = sad prediction
+# (8.5 * 0.1) + (0.65 * 0.1) + (1.2 * -0.3) = 0.555 = hurt prediction => dot(inp,matrix[0])
+# (8.5 * 0.1) + (0.65 * 0.2) + (1.2 * 0.0) = 0.98 = win prediction    => dot(inp,matrix[1])
+# (8.5 * 0.0) + (0.65 * 1.3) + (1.2 * 0.1) = 0.965 = sad prediction   => dot(inp,matrix[2])
